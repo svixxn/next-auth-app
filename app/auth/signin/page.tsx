9a9c0d86ada1df4c7117ce070b570355b57a0a7a
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-08-25 01:30:50 +0300

    created simple next-auth app

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+import React from "react";
+
+const signIn = () => {
+  return (
+    <main>
+      <LoginForm isLogin={true} />
+    </main>
+  );
+};
+
+export default signIn;
